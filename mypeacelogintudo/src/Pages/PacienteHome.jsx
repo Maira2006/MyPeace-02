00b8079c100635c28ebb00cf127df47e44b1767f
paulohenrique <paulohenriquedeoliveira2006@gmail.com>
2024-05-18 18:41:42 -0300

    Add files via upload

---
+++ mypeacelogintudo/src/Pages/PacienteHome.jsx
@@ -0,0 +1,13 @@
+import Cabecalho from "../PacienteHome/cabecalho/Cabecalho";
+import Conteudo from "../PacienteHome/conteudo/conteudo";
+import Rodape from "../PacienteHome/rodape/rodape";
+
+export default function PacienteHome(){
+    return(
+        <>
+            <Cabecalho/>
+            <Conteudo/>
+            <Rodape/>
+        </>
+    )
+}
