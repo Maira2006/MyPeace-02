04191c8c8f61eff91a95f5a766954249e47ffe0a
paulohenrique <paulohenriquedeoliveira2006@gmail.com>
2024-05-18 20:47:32 -0300

    Update Cabecalho.jsx

--- mypeacelogintudo/src/PacienteHome/cabecalho/Cabecalho.jsx
+++ mypeacelogintudo/src/PacienteHome/cabecalho/Cabecalho.jsx
@@ -1,22 +1,18 @@
-
-import'./Cabecalho.css'
-import logo from '../assets/logo.png'
-
-function Cabecalho(){
-    return(
-        <>
-            <header className="cabecalho_header">
-                <div className="nomeapp">
-                    <img src={logo} alt="Logo"></img>
-                    <h1>MyPeace</h1>
-                </div>
-            </header>    
-
-   
-
-        
-        </>
+import './Cabecalho.css'
+function Cabecalho() {
+    return (
+        <header>
+            <div>
+                <img src="Logo.png" alt="" className='my' /> 
+                <h1>MyPeace</h1>
+                <nav>
+                    <ul>
+                    <li><a href="#">Home</a></li>
+                </ul>
+                </nav>
+            </div>
+        </header>
     )
 }
 
-export default Cabecalho;
+export default Cabecalho
